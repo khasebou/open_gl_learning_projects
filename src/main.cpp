b68f240c824aed0da7c0270eb774d9450b9d0653
karim hasebou <karim.hasebou@gmail.com>
2024-03-01 10:33:18 +0100

    Handling error from GLFW init

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,10 @@ int main()
 {
     // glfw: initialize and configure
     // ------------------------------
-    glfwInit();
+    if( glfwInit() == GLFW_FALSE ) {
+        std::cout<< "Failed to init GLFW" << std::endl;
+        return -1;
+    }
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
